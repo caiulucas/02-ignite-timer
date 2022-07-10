b541e69a45ff66623a6ae23ce10316d5a2d452ec
Caio Lucas <lienscarlet@gmail.com>
2022-07-09 22:36:48 -0300

    feat: add Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,25 @@
+import { Scroll, Timer } from 'phosphor-react'
+
+import { HeaderContainer } from './styles'
+
+import { NavLink } from 'react-router-dom'
+import LogoIgnite from '../../assets/logo-ignite.svg'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img
+        src={LogoIgnite}
+        alt="Ignite Logo (Two green triangles on vertical)"
+      />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
