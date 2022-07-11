73f88dc07d63778444fc42a40794942d941a4ef6
Caio Lucas <lienscarlet@gmail.com>
2022-07-11 00:27:46 -0300

    style: add StopCountdownButton styles

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -84,7 +84,7 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -97,19 +97,30 @@ export const StartCountdownButton = styled.button`
   gap: 0.5rem;
 
   font-weight: 700;
-  background-color: ${({ theme }) => theme['green-500']};
   color: ${({ theme }) => theme['gray-100']};
 
   cursor: pointer;
 
   transition: all 0.3s;
 
-  &:enabled:hover {
-    background-color: ${({ theme }) => theme['green-700']};
-  }
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
+  }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background-color: ${({ theme }) => theme['green-500']};
+
+  &:enabled:hover {
+    background-color: ${({ theme }) => theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background-color: ${({ theme }) => theme['red-500']};
+
+  &:enabled:hover {
+    background-color: ${({ theme }) => theme['red-700']};
   }
 `
