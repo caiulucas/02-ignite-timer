d2f889c7e66c834db3adb4416e3b266da8d94752
Caio Lucas <lienscarlet@gmail.com>
2022-07-11 15:04:01 -0300

    refactor: create cycles hook

---
+++ src/contexts/CyclesContext.tsx
@@ -0,0 +1,100 @@
+import { createContext, ReactNode, useContext, useState } from 'react'
+
+interface CreateCycleData {
+  task: string
+  minutesAmount: number
+}
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+  startDate: Date
+  interruptedDate?: Date
+  finishedDate?: Date
+}
+
+interface CyclesContextType {
+  activeCycle: Cycle | undefined
+  amountSecondsPassed: number
+  markCurrentCycleAsFinished: () => void
+  setSecondsPassed: (seconds: number) => void
+  createNewCycle: (data: CreateCycleData) => void
+  interruptCurrentCycle: () => void
+}
+
+export const CyclesContext = createContext<CyclesContextType>(
+  {} as CyclesContextType,
+)
+
+interface CyclesContextProviderProps {
+  children: ReactNode
+}
+
+export function CyclesContextProvider({
+  children,
+}: CyclesContextProviderProps) {
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  function markCurrentCycleAsFinished() {
+    setCycles((state) =>
+      state.map((cycle) =>
+        cycle === activeCycle ? { ...cycle, isFinished: true } : cycle,
+      ),
+    )
+
+    setActiveCycleId(null)
+  }
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds)
+  }
+
+  function createNewCycle(data: CreateCycleData) {
+    const newCycle: Cycle = {
+      id: String(new Date().getTime()),
+      startDate: new Date(),
+      ...data,
+    }
+
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(newCycle.id)
+    // reset()
+  }
+
+  function interruptCurrentCycle() {
+    setCycles((state) =>
+      state.map((cycle) =>
+        cycle === activeCycle
+          ? { ...cycle, interruptedDate: new Date() }
+          : cycle,
+      ),
+    )
+    setActiveCycleId(null)
+    setAmountSecondsPassed(0)
+    document.title = 'Ignite Timer'
+  }
+
+  return (
+    <CyclesContext.Provider
+      value={{
+        activeCycle,
+        amountSecondsPassed,
+        markCurrentCycleAsFinished,
+        setSecondsPassed,
+        createNewCycle,
+        interruptCurrentCycle,
+      }}
+    >
+      {children}
+    </CyclesContext.Provider>
+  )
+}
+
+export function useCycles() {
+  return useContext(CyclesContext)
+}
